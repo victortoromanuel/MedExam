3f4e570779940c0b6cb0dfaa8237ec2fab8cd720
Victor Manuel Toro Cedeño <vmtoro@constructoramelendez.com>
2022-05-25 23:10:39 -0500

    pago completado en examen corto

--- src/app/corto/corto.component.ts
+++ src/app/corto/corto.component.ts
@@ -26,7 +26,7 @@ export class CortoComponent implements OnInit {
         console.log("el corto si si");
         console.log(data);
     });
-    this.epayco = "<form><script src='https://checkout.epayco.co/checkout.js' data-epayco-key='437506cc0cb092ec55496b7d67cedd6f' class='epayco-button' data-epayco-amount='30000' data-epayco-tax='0.00' data-epayco-tax-ico='0.00' data-epayco-tax-base='30000' data-epayco-name='Examen corto' data-epayco-description='Examen corto' data-epayco-currency='cop' data-epayco-country='CO' data-epayco-test='true' data-epayco-external='false' data-epayco-response='http://localhost:4200/#/pago/1/corto' data-epayco-acepted='http://localhost:4200/#/pago/"+ this.route.snapshot.paramMap.get('id') +"/corto' data-epayco-button='https://multimedia.epayco.co/dashboard/btns/btn5.png'></script><form>";
+    this.epayco = "<form><script src='https://checkout.epayco.co/checkout.js' data-epayco-key='437506cc0cb092ec55496b7d67cedd6f' class='epayco-button' data-epayco-amount='30000' data-epayco-tax='0.00' data-epayco-tax-ico='0.00' data-epayco-tax-base='30000' data-epayco-name='Examen corto' data-epayco-description='Examen corto' data-epayco-currency='cop' data-epayco-country='CO' data-epayco-test='true' data-epayco-external='false' data-epayco-method='GET' data-epayco-response='http://localhost:4200/#/pago/1/corto'  data-epayco-button='https://multimedia.epayco.co/dashboard/btns/btn5.png'></script><form>";
     postscribe('#epay', this.epayco);
     
   }
