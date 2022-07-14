b1d01826f3bf63af389221cb9ce3ccfb534d117f
Isabela Acevedo García <44243406+isa43461@users.noreply.github.com>
2022-07-14 10:23:36 -0500

    update

--- src/app/aterrizaje/aterrizaje.component.ts
+++ src/app/aterrizaje/aterrizaje.component.ts
@@ -11,5 +11,14 @@ export class AterrizajeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+   myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 }
