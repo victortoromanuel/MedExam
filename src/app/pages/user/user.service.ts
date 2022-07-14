ebb5a29e079750f4d481da0fec61c4f0ae1114c1
victortoromanuel <victortoromanuel@gmail.com>
2022-07-13 22:31:28 -0500

    Cambios para el deploy

--- src/app/pages/user/user.service.ts
+++ src/app/pages/user/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { UsuarioI } from 'app/models/Usuario/Usuario.interface';
+//import { UsuarioI } from 'app/models/Usuario/Usuario.interface';
 import { Observable } from 'rxjs';
 
 @Injectable({
